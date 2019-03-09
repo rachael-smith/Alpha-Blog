810aac3e87d3cb747e41c7831eb4a890fb08745a
Rachael Smith <rachaelsmith@Rachaels-Air.lan1>
2019-03-09 09:31:43 -0500

    add partials and destory method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,47 @@
 class ArticlesController < ApplicationController
-    def new
-        @article = Article.new
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def create
-        @article = Article.new(article_params)
-        if @article.save
-            flash[:notice] = "article was successfully created" 
-            redirect_to article_path(@article)
-        else
-            render 'new'
-        end
-    end
+  def new
+    @article = Article.new
+  end
 
-    def show
-        @article = Article.find(params[:id])
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
     end
+  end
 
-    private def article_params
-        params.require(:article).permit(:title,:description)
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Article was successfully deleted"
+  end
+  private def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
