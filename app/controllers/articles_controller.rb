a8108eeca6a23c8d21a4be56ef6291f1c8ae8bf4
Rachael Smith <rachaelsmith@Rachaels-Air.lan1>
2019-03-09 09:37:52 -0500

    clean up redundencies

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article_by_id, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -18,15 +20,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -36,12 +35,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
     flash[:notice] = "Article was successfully deleted"
   end
-  private def article_params
+  private
+  def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article_by_id
+    @article = Article.find(params[:id])
   end
 end
