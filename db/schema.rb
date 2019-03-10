cd61c60600aee25765431c91f860c0c7661d5651
Rachael Smith <rachaelsmith@Rachaels-Air.lan1>
2019-03-09 19:41:10 -0500

    added passwords and create and edit user pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_224731) do
+ActiveRecord::Schema.define(version: 2019_03_09_234959) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_224731) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
